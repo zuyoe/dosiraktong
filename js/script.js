5fc561bef7ae8a31a7c5294893e4e5527a0e56f8
zuyoe <9xtwo5@naver.com>
2023-04-11 17:30:00 +0900

    모달 추가

--- js/script.js
+++ js/script.js
@@ -1,12 +1,51 @@
 window.onload = function () {
-  // 모달창 기능
-  let modalWrap = document.querySelector(".modal-wrap");
-  let modalClose = document.querySelector(".modal-close");
-  modalClose.addEventListener("click", function () {
-    modalWrap.classList.add("fadeOut");
-    modalWrap.addEventListener("animationend", () => {
-      modalWrap.style.display = "none";
-    });
+  // ** FADE OUT FUNCTION **
+    // fadeOut( element : document.querySelctor(대상) )
+    function fadeOut(el) {
+      // 대상.style.투명도 = 불투명
+      el.style.opacity = 1;
+      (function fade() {
+          // 대상.style.투명도 -= 0.1 감소
+          if ((el.style.opacity -= 0.1) < 0) {
+              el.style.display = "none";
+          } else {
+              // 웹브라우저 프레임갱신
+              requestAnimationFrame(fade);
+          }
+      })();
+  }
+
+  // ** FADE IN FUNCTION **
+  // fadeIn( element : document.querySelctor(대상) )
+  function fadeIn(el, display) {
+      el.style.opacity = 0;
+      el.style.display = display || "block";
+      (function fade() {
+          var val = parseFloat(el.style.opacity);
+          if (!((val += 0.1) > 1)) {
+              el.style.opacity = val;
+              requestAnimationFrame(fade);
+          }
+      })();
+  }
+
+  // 모달창
+  let body = document.querySelector("body");
+  let modal = document.querySelector(".modal");
+  modal.addEventListener("click", function () {
+      // modal.style.display = "none";
+      // fadeOut(modal);
+      anime({
+          targets: ".modal",
+          delay: 200,
+          duration: 500,
+          opacity: 0,
+          easing: "easeInOutQuad",
+          complete: function () {
+              modal.style.display = "none";
+              body.classList.add("active");
+          },
+      });
   });
 
   // AOS 셋팅
